5a86b099586f7a005fe088855ce713d462602fad
Allie Pacyga <47311633+pallie1@users.noreply.github.com>
2020-11-16 17:46:25 -0600

    Updates

---
+++ precache-manifest.a6cbcd83be52de18deb96669bea6f921.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1328e2b73d6f5874b87e40117792997f",
+    "url": "/animalcross/index.html"
+  },
+  {
+    "revision": "b459e779312cb8ac126c",
+    "url": "/animalcross/static/css/main.d0791e35.chunk.css"
+  },
+  {
+    "revision": "8918964459c396bb16b7",
+    "url": "/animalcross/static/js/2.fa7d3427.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/animalcross/static/js/2.fa7d3427.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b459e779312cb8ac126c",
+    "url": "/animalcross/static/js/main.e22a0a6a.chunk.js"
+  },
+  {
+    "revision": "2901f0af4e9c81f39c05",
+    "url": "/animalcross/static/js/runtime-main.13363b16.js"
+  }
+]);
